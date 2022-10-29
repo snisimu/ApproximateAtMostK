9e7a4240f24b5981f3c9c8bc2ada44cd1f1b50e7
Shunji Nishimura <s-nishimura@oita-ct.ac.jp>
2022-10-29 17:44:13 +0900

    (work in progress)

--- src/Base.hs
+++ src/Base.hs
@@ -11,6 +11,13 @@ combinations xs n | n > 0 =
         | otherwise = map (x:) (go (n'-1) b p x's)
                         ++ go n' b (tail p) x's
 
+allFTssOf n = filter (\ss -> length ss == n) allFTss
+  where
+  allFTss = if n == 0 then [[]] else [] : makeFTss (n - 1) [[False],[True]]
+  makeFTss n bss = if n == 0
+    then bss
+    else makeFTss (n - 1) $ bss ++ [ bs ++ [False] | bs <- bss ] ++ [ bs ++ [True] | bs <- bss ]
+
 data Var a
     = X Int
     | VarNew a
--- src/Binary.hs
+++ src/Binary.hs
@@ -6,7 +6,7 @@ module Binary where
 import Base
 
 data Vnew
-    = Phi Int Int Int
+    = B Int Int
     | T Int Int
     deriving (Eq, Show)
 
@@ -15,12 +15,14 @@ atMost (k, n) = cnf
     where
     theMax i = max 1 $ k - n + i
     theMin i = min i k
-    log2n = head $ filter (\i -> n <= 2^i) [1..] -- floor (logBase 2 n) + 1
-    cnf =
+    log2n = head $ filter (\i -> n <= 2^i) [1..] -- floor (logBase 2 n) + if ..
+    sOf i = allFTssOf log2n !! (i - 1)
+    phi i g j = (sOf i !! (j + 1), VarNew $ B g j)
+    cnf = 
         let a = flip map [1..n] \i ->
                     (:) (False, X i) $
                         flip map [theMax i .. theMin i] \g -> (True, VarNew $ T g i)
             b = flip concatMap [1..n] \i ->
                     flip concatMap [theMax i .. theMin i] \g ->
-                        flip map [1..log2n] \j -> [(False, VarNew $ T g i), (True, VarNew $ Phi i g j)]
+                        flip map [1..log2n] \j -> [(False, VarNew $ T g i), phi i g j]
         in  a ++ b
