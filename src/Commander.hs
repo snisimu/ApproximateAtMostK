cfc476b1442dccc48857f4aa04169c88b133e2f7
Shunji Nishimura <s-nishimura@oita-ct.ac.jp>
2022-11-08 11:54:34 +0900

    introduce mNext for commander

--- src/Commander.hs
+++ src/Commander.hs
@@ -12,35 +12,38 @@ import Data.Maybe
 import Base
 import Binomial
 
-mFor :: [Literal] -> Int
-mFor xs = length xs `div` 10 + 1
+s :: Int
+s = 10
 
 commander :: NumberConstraint
-commander x's k' = command [] (Just $ mFor x's) x's k'
+commander x's k' = command [] Nothing x's k'
     where
     command :: [ScopeID] -> Maybe Int -> NumberConstraint
-    command sIDs mbM xs k = 
-        let m = fromMaybe (mFor xs) mbM
-        in  if m <= 1
-            then binomial xs k
-            else 
-                let hss = divideInto m [1..n]
-                    g = length hss
-                    n = length xs
-                    c i j = (True, C sIDs i j)
-                    c1 = flip concatMap [1..g] \i -> 
-                            let lits
-                                  =  [ xs !! (h-1) | h <- hss !! (i-1) ]
-                                  ++ [ not $ c i j | j <- [1..k] ]
-                            in  command ("c1AM" : sIDs) Nothing lits k
-                                ++ atLeastBy (command ("c1AL" : sIDs) Nothing) lits k
-                    c2 =
-                        [ [not $ c i j, c i (j+1)]
-                        | i <- [1..g]
-                        , j <- [1..k-1]
-                        ]
-                    c3 = command ("c3" : sIDs) (Just $ m - 1) [ c i j | i <- [1..g], j <- [1..k] ] k
-                in  c1 ++ c2 ++ c3
+    command sIDs mbM xs k = if m' <= 1
+        then binomial xs k
+        else 
+            let hss = divideInto m [1..n]
+                g = length hss
+                n = length xs
+                c i j = (True, C sIDs i j)
+                mNext =
+                    let m' = length xs `div` s + 1
+                    in  Just $ maybe m' $ min m'
+                c1 = flip concatMap [1..g] \i -> 
+                        let lits
+                                =  [ xs !! (h-1) | h <- hss !! (i-1) ]
+                                ++ [ not $ c i j | j <- [1..k] ]
+                        in  command ("c1AM" : sIDs) mNext lits k
+                            ++ atLeastBy (command ("c1AL" : sIDs) mNext) lits k
+                c2 =
+                    [ [not $ c i j, c i (j+1)]
+                    | i <- [1..g]
+                    , j <- [1..k-1]
+                    ]
+                c3 =
+                    let lits = [ c i j | i <- [1..g], j <- [1..k] ]
+                    in  command ("c3" : sIDs) mNext lits k
+            in  c1 ++ c2 ++ c3
 
 commanderIO x's k' = commandIO [] (Just $ mFor x's) x's k'
     where
