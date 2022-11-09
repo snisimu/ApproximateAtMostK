3145cd475c781d5e85548c94ef1df61c9db9f0a3
Shinji Nishimura <snisimu@gmail.com>
2022-11-09 20:43:20 +0900

    close #21, #22

--- src/Approximate.hs
+++ src/Approximate.hs
@@ -22,71 +22,68 @@ approximate xs k =
     in  []
 
 allIsss :: Int -> Int -> [[[Int]]]
-allIsss m l = makeIsss (l-1) $ return $ map return [1..m]
+allIsss w d = makeIsss (d-1) $ return $ map return [1..w]
   where
   makeIsss :: Int -> [[[Int]]] -> [[[Int]]]
-  makeIsss l isss = if l == 0
+  makeIsss d isss = if d == 0
     then isss
-    else makeIsss (l-1) $ isss ++ [[ is ++ [i] | is <- last isss, i <- [1..m] ]]
-allIss m l = concat $ allIsss m l
+    else makeIsss (d-1) $ isss ++ [[ is ++ [i] | is <- last isss, i <- [1..w] ]]
+allIss w d = concat $ allIsss w d
 
-approx :: (Int, Int, Int) -> (CNF, [[[Int]]])
+approx :: (Int, Int, Int) -> (CNF, [[Int]])
 approx (h, w, d) =
   let n = h * w^d
-    p is j = (True, P is j)
-    iss = allIss w d
-    order = flip concatMap iss \is ->
-      flip map [2..h] \j ->
-        [ not $ p is j, p is $ j-1 ]
-    atMost = flip concatMap iss \is ->
-      let ps = p <$> filter ((==) is . init) iss <*> [1..h]
-      in  flip concatMap [1..h] \j ->
-            map ((:) $ p is j) $ binomial ps $ w*(j-1)
-    isLeafss =
-      let n = maximum $ map length iss
-      in  chunksOf w $ filter ((==) n . length) iss
-  in  (order ++ atMost, isLeafss)
+      p is j = (True, P is j)
+      iss = allIss w d
+      order = flip concatMap iss \is ->
+        flip map [2..h] \j ->
+          [ not $ p is j, p is $ j-1 ]
+      atMost = flip concatMap iss \is ->
+        let ps = p <$> filter ((==) is . init) iss <*> [1..h]
+        in  flip concatMap [1..h] \j ->
+              map ((:) $ p is j) $ binomial ps $ w*(j-1)
+      isLeafs =
+        let n = maximum $ map length iss
+        in  filter ((==) n . length) iss
+  in  (order ++ atMost, isLeafs)
 
 approxWithX :: (Int, Int, Int) -> CNF
-approxWithX (a, m, l) =
-  let (cnfP, isLeafss) = approx (a, m, l)
-      xss = splitBy a $ literalXs $ (length isLeafs) * a
+approxWithX (h, w, d) =
+  let p is j = (True, P is j)
+      (cnfP, isLeafs) = approx (h, w, d)
+      xss = splitBy (h*w) $ literalXs $ (length isLeafs) * h*w
       cnfX = flip concatMap (zip isLeafs xss) \(is, xs) -> 
-        flip concatMap [1..a] \j ->
-          map ((:) $ p is j) $ binomial xs $ m*(j-1)
+        flip concatMap [1..h] \j ->
+          map ((:) $ p is j) $ binomial xs $ w*(j-1)
   in  cnfP ++ cnfX
 
+-- > generateDIMACSwithTrue (approxWithX (2,2,1) ++ binomial [ (True, P [i] j) | i <- [1..2], j <- [1..2] ] 2) [1,2,3,4]
+
 isPossible :: (Int, Int, Int) -> [Int] -> Int -> IO Bool
-isPossible (a, m, l) js k = do
-  unless (k < a*m) $ die "k: too large"
-  let n = a * m^l
+isPossible (h, w, d) js k = do
+  unless (k < h*w) $ die "k: too large"
+  let n = h * w^d
   unless (null $ filter (> n) js) $ die "js: out of range"
-  let isss = allIsss m l
-      bss = splitBy a $ foldr makeTrueAt (replicate n False) js
+  let isss = allIsss w d
+      bss = splitBy h $ foldr makeTrueAt (replicate n False) js
         where
-        makeTrueAt h bs =
-          let (hd, tl) = splitAt h bs
+        makeTrueAt m bs =
+          let (hd, tl) = splitAt m bs
           in  init hd ++ [True] ++ tl
       integrate :: [Int] -> Int
-      integrate hs = case integr [] hs of
+      integrate ls = case integr [] ls of
         [z] -> z
-        h's -> integrate h's
+        l's -> integrate l's
         where
         integr gs = \case
           [] -> gs
           hs ->
-            let (hHD, hTL) = splitAt m hs
-                (y, z) = sum hHD `divMod` m
+            let (hHD, hTL) = splitAt w hs
+                (y, z) = sum hHD `divMod` w
                 r = y + if z == 0 then 0 else 1
             in  integr (gs ++ [r]) hTL
       z = integrate $ map (length . filter id) bss
   return $ z < k
-
--- > generateDIMACSwithTrue (approxWithX (2,2,3) ++ binomial [ (True, P [i] j) | i <- [1..2], j <- [1..2] ] 2) [1,2,3]
-
--- > generateDIMACSwithTrue (approx (2,2,3) ++ binomial [ (True, P [i] j) | i <- [1..2], j <- [1..2] ] 2) [1,2,3]
--- NG: [1,2,3],[1,2,4],[2,3,4]
--- OK: [1,2],[1,2,5,6],[1,2,7,8]
 
 {-
 countObjBss n =
