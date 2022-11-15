3f13186138934ac59eb373e9ae8d145a15e387ee
Shunji Nishimura <s-nishimura@oita-ct.ac.jp>
2022-11-15 15:18:47 +0900

    close #36

--- src/Approximate/Encoding.hs
+++ src/Approximate/Encoding.hs
@@ -29,10 +29,11 @@ labeling = tail . concat . foldl makeH'Isss [[([], 0)]]
   makeH'Isss ishss (h, w) =
     ishss ++ [[ (is ++ [i], h) | (is, _) <- last ishss, i <- [1..w] ]]
 
-approxPwith :: NumberConstraint -> VarScope -> [HW] -> (CNF, (m, [[Int]]))
-approxPwith atMost vScope .ghws =
-  let p is j = (True, vScope $ P is j)
-      is'hs = labeling hws
+approxPwith :: NumberConstraint -> VarScope -> [HW] -> (CNF, (Height, [[Int]]))
+approxPwith atMost vScope hws = 
+  let hw's = init hws
+      p is j = (True, vScope $ P is j)
+      is'hs = labeling hw's
       cnfOrder = flip concatMap is'hs \(is, h) ->
         flip map [2..h] \j ->
           [ not $ p is j, p is $ j-1 ]
@@ -45,24 +46,26 @@ approxPwith atMost vScope .ghws =
               let theScope = vScope . Scope ("approxPwith:" ++ show is ++ show j)
               in  map ((:) $ p is j) $ 
                     atMost theScope ps $ (h'*w'*(j-1)) `div` h
+      hLeaf = if null hw's then 1 else fst $ last hw's
       isLeafs =
         let iss = map fst is'hs
-        in  filter ((==) (length hws) . length) iss
-  in  (cnfOrder ++ cnfAtMost, isLeafs)
+        in  filter ((==) (length hw's) . length) iss
+  in  (cnfOrder ++ cnfAtMost, (hLeaf, isLeafs))
 
 approxWith :: NumberConstraint -> VarScope -> [HW] -> Int -> CNF
 approxWith atMost vScope hws k =
   let vScopeNext sID = vScope . Scope ("approxWith:" ++ sID)
       p is j = (True, vScope $ P is j)
       (h, w) = head hws
-      cnfTop = atMost (vScopeNext "top") [ (True, P [i] j) | i <- [1..w], j <- [1..h] ] k
-      (cnfP, isLeafs) = approxPwith atMost vScope{- (vScopeNext "P") -} hws
-      (h', w') = last hws
-      m = product $ map snd $ init hws
-      xss = splitBy h' $ literalXs $ h'*w'*m
-      cnfX = flip concatMap (zip isLeafs xss) \(is, xs) -> 
-        flip concatMap [1..h'] \j ->
-          map ((:) $ p is j) $ atMost (vScopeNext $ "X:" ++ show is ++ show j) xs $ j-1
+      cnfTop = atMost (vScopeNext "top") [ p [i] j | i <- [1..w], j <- [1..h] ] k
+      (cnfP, (hLeaf, isLeafs)) = approxPwith atMost vScope hws
+      cnfX =
+        let (h', w') = last hws
+            m = product $ map snd $ init hws
+            xss = splitBy (h'*w') $ literalXs $ h'*w'*m
+        in  flip concatMap (zip isLeafs xss) \(is, xs) -> 
+              flip concatMap [1..hLeaf] \j ->
+                map ((:) $ p is j) $ atMost (vScopeNext $ "X:" ++ show is ++ show j) xs $ ((j-1)*h'*w') `div` hLeaf
   in  cnfTop ++ cnfP ++ cnfX
   -- > generateDIMACSwithTrue (approxWith counter id [(2,2),(2,2)] 2) [1,2,3,4]
-
+  -- > wsl -- ./minisat the.cnf
