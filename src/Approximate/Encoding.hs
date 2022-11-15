5dc2d340ed0b4906846ca52117201674c0cdc36f
Shunji Nishimura <s-nishimura@oita-ct.ac.jp>
2022-11-15 14:26:31 +0900

    arrange

--- src/Approximate/Encoding.hs
+++ src/Approximate/Encoding.hs
@@ -30,7 +30,7 @@ labeling = tail . concat . foldl makeH'Isss [[([], 0)]]
     ishss ++ [[ (is ++ [i], h) | (is, _) <- last ishss, i <- [1..w] ]]
 
 approxPwith :: NumberConstraint -> VarScope -> [HW] -> (CNF, (m, [[Int]]))
-approxPwith atMost vScope hws =
+approxPwith atMost vScope .ghws =
   let p is j = (True, vScope $ P is j)
       is'hs = labeling hws
       cnfOrder = flip concatMap is'hs \(is, h) ->
