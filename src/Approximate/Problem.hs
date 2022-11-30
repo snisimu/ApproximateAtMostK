5e1496ac96315f8ed979ab110d9f662d7264a23c
Shunji Nishimura <snisimu@gmail.com>
2022-11-30 06:58:28 +0000

    wip

--- src/Approximate/Problem.hs
+++ src/Approximate/Problem.hs
@@ -7,17 +7,44 @@ import Control.Monad
 
 import Text.Printf
 
-a = 2
+import Base
+
+a = 2 -- need?
 pN = 10/100
 pK = 1/100
 
-solutionNum (k,n) i =
-    let n' = fromInteger $ toInteger n
+solutionNums :: KN -> [Int]
+solutionNums (k,n) =
+    let limitSolutionNum = 10000 :: Int
+        n' = fromInteger $ toInteger n
         k' = fromInteger $ toInteger k
-        i' = fromInteger $ toInteger i
-        p' = (pN-pK) / (n'-k') * i' - (pN-pK) / (n'-k') * n' + pN
-        p = if p' < 0
-            then 0
-            else p'
-    in  p * solutionNumAll
-    -- > forM_ [0..n] $ \i -> putStrLn (show i ++ ": " ++ printf "%.8f" (pOf i))
+        p i =
+            let i' = fromInteger $ toInteger i
+                p' = (pN-pK) / (n'-k') * i' - (pN-pK) / (n'-k') * n' + pN
+            in  if p' < 0
+                then 0
+                else p'
+        nn = combinationNum False (n, n)
+        n's = flip map [0..n] \i -> floor $ p i * fromInteger (toInteger $ combinationNum False (i, n))
+        n'max = maximum n's
+        r = if limitSolutionNum < n'max
+            then fromInteger (toInteger limitSolutionNum) / fromInteger (toInteger n'max)
+            else 1
+    in  map (floor . (*) r . fromInteger . toInteger) n's
+
+generateProblem :: KN -> IO () 
+generateProblem (k, n) = do
+    let ns = solutionNums (k, n)
+    cnfs <- concat <$> for [0..n] \k -> do
+        let n = ns !! k
+            defNewSolution s's = do
+
+            defineSolution s's = do
+                if n <= length s's
+                    then return s's
+                    else do
+                        s <- defNewSolution s's
+                        defineSolution $ s : s's
+        ss <- defineSolution []
+
+        return cnfs
