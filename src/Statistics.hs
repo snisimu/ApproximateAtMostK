da7e74ebda669fbb7237d270da580336f7cde268
Shinji Nishimura <snisimu@gmail.com>
2022-12-05 22:32:20 +0900

    close #53

--- src/Statistics.hs
+++ src/Statistics.hs
@@ -5,6 +5,8 @@ module Statistics where
 
 import Control.Monad
 
+import Text.Printf
+
 import Base
 import Lib
 import Evaluation
@@ -15,7 +17,7 @@ solutionNumsForExampleRate (k, n) r = reverse $
     let m = combinationNum True (k', n)
     in  (k', floor $ r' * fromInteger (toInteger m))
 
-exampleRate :: ParameterCNF -> Float -> IO ()
+exampleRate :: ParameterCNF -> Float -> IO Float
 exampleRate paramCNF r = do
   let (k, n) = knOfSpace paramCNF
   let kNums = solutionNumsForExampleRate (k, n) r
@@ -25,6 +27,31 @@ exampleRate paramCNF r = do
     return $ (1-p)^num
   -- print ps -- [debug]
   let rate = 1 - product ps
-  print rate
-  -- > exampleRate ((([(2,3)],2),3),(1,1)) 10 -- (5,10)
-  -- > forM_ [1..10] $ \i -> exampleRate ((([(2,3)],2),3),(1,1)) $ 0.01 * fromInteger i
+  return rate
+  -- > exampleRate ((([(2,2),(2,2)],2),2),(3,3)) 0.01 -- (5,10)
+  -- > exampleRate ((([(2,2),(2,3)],2),2),(2,2)) 0.01 -- (10,20)
+
+nonZeroTo99 :: ParameterCNF -> IO (Float, Float)
+nonZeroTo99 paramCNF = do
+  pNonZero <- seekNZ 0.0001
+  p99 <- seek99 0.01
+  return (pNonZero, p99)
+  where
+    seekNZ r = do
+      p <- exampleRate paramCNF r
+      if 0 < p
+        then return r
+        else seekNZ $ r + 0.0001
+    seek99 r = do
+      p <- exampleRate paramCNF r
+      if 0.99 < p
+        then return r
+        else seek99 $ r + 0.01
+
+exampleRates :: ParameterCNF -> Float -> Int -> IO ()
+exampleRates paramCNF d nIterate = do
+  forM_ [1..nIterate] \i -> do
+    let r = d * fromInteger (toInteger i)
+    rate <- exampleRate paramCNF r
+    putStrLn $ printf "%.8f" r ++ ": " ++ show rate
+  -- > exampleRates ((([(2,2),(2,3)],2),2),(2,2)) 0.000005 20
