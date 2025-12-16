106a1854bd3c03d91c3703cdd5963cc8472cc0c1
Shinji Nishimura <snisimu@gmail.com>
2025-12-16 18:20:01 +0900

    pigeon hole: (3, 4) -> SAT, (3, 5) -> UNSAT !!

--- src/PigeonHole.hs
+++ src/PigeonHole.hs
@@ -22,7 +22,7 @@ import Evaluation
 -- add target literals
 approxPracticalWith :: NumberConstraint -> VarScope -> ParameterCNF -> [Literal] -> CNF
 approxPracticalWith atMost vScope paramCNF xIns =
-  let xIns' = xIns ++ replicate nFalse (False, vScope $ V "false") ++ replicate nTrue (True, vScope $ V "true")
+  let xIns' = xIns ++ replicate nFalse (True, vScope $ V "false") ++ replicate nTrue (True, vScope $ V "true")
       (((hws, m), k), (nFalse, nTrue)) = paramCNF
       vScopeNext sID = vScope . Scope ("approxOrderWith:" ++ sID)
       p is j = (True, vScope $ P is j)
@@ -43,21 +43,23 @@ approxPracticalWith atMost vScope paramCNF xIns =
       paramT = (hws, m)
       (kT, nT) = knOfTree (paramT, k)
       cnfAppr = approxOrderWith binomial id (paramT, k)
-      cnfFix = map (\i -> [xIns' !! (i-1)]) [nT-nTrue-nFalse+1..nT-nTrue]
+      cnfFix = map (\i -> [(False, snd $ xIns' !! (i-1))]) [nT-nTrue-nFalse+1..nT-nTrue]
           ++ map (\i -> [xIns' !! (i-1)]) [nT-nTrue+1..nT]
   in  cnfTop ++ cnfP ++ cnfX ++ cnfFix
 
-pigeonHole :: (Int, Int) -> IO CNF
-pigeonHole (l, m) = do
+pigeonHole :: Bool -> (Int, Int) -> IO CNF
+pigeonHole blDebug (l, m) = do
   let n = l * m
   (_, paramCNF) <- theBestEfficiency False True (l, l*m)
   putStrLn $ "Using parameterCNF: " ++ show paramCNF
   let approx vScope xs = approxPracticalWith binomial vScope paramCNF xs
       lV i j = literal $ V $ "p" ++ show i ++ "h" ++ show j
   let holeAM = concatMap (\j -> approx (Scope $ "holeAM" ++ show j) $ [ lV i j | i <- [1..n] ]) [1..m]
+      holeAMdebug = counter (Scope "holeAMdebug") [ lV i 1 | i <- [1..n] ] (l - 1)
       pigeonAM = concatMap (\i ->  (counter (Scope $ "pigeonAM" ++ show i) [ lV i j | j <- [1..m] ] 1)) [1..n]
       pigeonAL = map (\i -> [ lV i j | j <- [1..m] ]) [1..n]
-  pure $ holeAM {- ++ pigeonAM -} ++ pigeonAL
-  -- > pigeonHole (3, 3) >>= printCNF
-  -- > pigeonHole (3, 3) >>= printDIMACS
-  -- > pigeonHole (3, 3) >>= \cnf -> generateDIMACSwithTrue cnf []
+  pure $ holeAM ++ (if blDebug then holeAMdebug else []) ++ pigeonAM ++ pigeonAL
+  -- > pigeonHole (2, 3) >>= printCNF
+  -- > pigeonHole (2, 3) >>= printDIMACS
+  -- > pigeonHole (2, 3) >>= \cnf -> generateDIMACSwithTrue cnf []
+  -- (3, 4) -> SAT, (3, 5) -> UNSAT !!
